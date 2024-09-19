beacb6b2da333e292c575c7268a93cae3801036a
Pyorang <sudangi143@gmail.com>
2024-09-19 22:29:49 +0900

    FirstGit

---
+++ Assets/Test.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Awake() => Debug.Log("Two");
+
+    //void OnEnable() => Debug.Log($"{name}::{nameof(OnEnable)}");
+
+    //void Start() => Debug.Log($"{name}::{nameof(Start)}");
+
+    //void Update() => Debug.Log($"{name}::{nameof(Update)}");
+
+    //void LateUpdate() => Debug.Log($"{name}::{nameof(LateUpdate)}");
+
+    //void OnApplicationPause(bool pause) => Debug.Log($"{name}::{nameof(OnApplicationPause)}, {pause}");
+
+    //void OnApplicationQuit() => Debug.Log($"{name}::{nameof(OnApplicationQuit)}");
+
+    //void OnDisable() => Debug.Log($"{name}::{nameof(OnDisable)}");
+
+    //void OnDestroy() => Debug.Log($"{name}::{nameof(OnDestroy)}");
+}
---
+++ Assets/Test4.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test4 : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("Random?");
+    }
+}
